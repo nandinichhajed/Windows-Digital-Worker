93fc67400fba8bbb69cf8e12d1a60e4d12dbf135
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-10-04 13:09:35 +0530

    [Feat]: Service page created
    - Style the service page
    - Added service image

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,9 +1,55 @@
 import React from "react";
 
+import ServiceImage from "../img/servicepage.svg";
+import "./css/Service.css";
+
+const options = [
+  {
+    title: "Start",
+    value: "START",
+  },
+  {
+    title: "Stop",
+    value: "STOP",
+  },
+  {
+    title: "Pause",
+    value: "PAUSE",
+  },
+  {
+    title: "Resume",
+    value: "RESUME",
+  },
+];
+
 const Service = () => {
   return (
     <div>
-      <h1>Service Page</h1>
+      <div className="service-page">
+        <div className="content">
+          <img src={ServiceImage} alt="Monitor" />
+
+          <div className="buttonContainer">
+            <input
+              type="text"
+              name="service"
+              id="service"
+              placeholder="Enter the name of Service"
+            />
+            <form>
+              <select name="service" id="service">
+                {options &&
+                  options.map((opt) => (
+                    <option key={opt.name} value={opt.value}>
+                      {opt.title}
+                    </option>
+                  ))}
+              </select>
+            </form>
+            <p className="primary">Check</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
